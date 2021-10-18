e2a9e17c1b92f695c5c5c20b5524d72cc0c21778
Bajtix <bajtix.b@gmail.com>
2021-10-18 21:17:24 +0200

    dodano moduł wiadomości drobne poprawki nowe ikony
    Signed-off-by: Bajtix <bajtix.b@gmail.com>

---
+++ Karate/MessagesTabPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrusLib;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Karate {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessagesTabPage : ContentPage {
+        private App application;
+        
+        public MessagesTabPage() {
+            application = Application.Current as App;
+            InitializeComponent();
+
+            Task.Run(FetchMessages);
+        }
+        
+        public async Task FetchMessages() {
+            LibrusMessageReceiver receiver = await LibrusMessageReceiver.Retrieve(application.librusApi);
+            var messages = receiver.messages;
+            Device.InvokeOnMainThreadAsync(()=> {
+                //BindableLayout.SetItemsSource(MessageDisplay, messages);
+                MessageDisplay.ItemsSource = messages;
+            });
+        }
+
+        private void MessageDisplay_OnItemTapped(object sender, ItemTappedEventArgs e) {
+            Navigation.PushModalAsync(new ViewMessagePage((LibrusMessage)e.Item));
+        }
+    }
+}
---
+++ Karate/ViewMessagePage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrusLib;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Karate {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewMessagePage : ContentPage, INotifyPropertyChanged {
+
+        public LibrusMessage msg { get; set; }
+
+        public string Content {
+            get => content;
+            set {
+                OnPropertyChanged(nameof(Content));
+                content = value;
+            }
+        }
+
+        public string Author { get; set; }
+        public string Date { get; set; }
+        public string Title { get; set; }
+
+        private LibrusConnection connection;
+        private string content;
+
+        public ViewMessagePage() {
+            InitializeComponent();
+        }
+        
+        public ViewMessagePage(LibrusMessage msg) {
+            InitializeComponent();
+            connection = (Application.Current as App).librusApi;
+            this.msg = msg;
+            Load();
+            Title = msg.Title;
+            Date = msg.ReceiveDate.ToString("dd.MM.yyyy HH:mm");
+            Author = msg.Author;
+            
+            BindingContext = this;
+        }
+
+        private async void Load() {
+            await msg.ReceiveContent(connection, APIBufferMode.none);
+
+            await Device.InvokeOnMainThreadAsync(() => {
+                ContentLabel.Text = msg.Content;
+                Content = msg.Content;
+            });
+        }
+    }
+}
